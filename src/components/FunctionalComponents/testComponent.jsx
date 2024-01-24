2490d8b92cd9add5ce88182cab67b0524b50ea3d
praneethapr <praneethapr.21ece@kongu.edu>
2024-01-24 23:20:13 +0530

    first commit

---
+++ src/components/FunctionalComponents/testComponent.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function TestComponent(){
+    return(
+        <div>
+            <h1>THIS IS A TEST FUNCTION COMPONENT.</h1>
+        </div>    
+    )
+}
+
+export default TestComponent;                                                      
